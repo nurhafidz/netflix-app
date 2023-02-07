0f9fc6ccc4c5e327ad9d3b80db6d375318f3f587
nurhafidz <nurhafidzfaizal03@gmail.com>
2023-02-07 22:26:14 +0700

    add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+
             <head />
             <body className={inter.className}>{children}</body>
         </html>
